7a78823ae67d78b9a8fbc5332d872c32c5a1263a
imad ziani <imad_ziani@live.fr>
2018-04-30 23:45:00 +0100

    message

--- PFE/src/pfe/PFE.java
+++ PFE/src/pfe/PFE.java
@@ -20,7 +20,7 @@ public class PFE extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("homePage.fxml"));
         
         Scene scene = new Scene(root);
        
